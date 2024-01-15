6365cf28f3fbc29efc0a8f1af12968c18c72812a
Sergei Bykov <ss.bykoff@gmail.com>
2024-01-15 11:08:04 +0300

    поправил метод formatCount

--- app/src/main/java/ru/netology/nmedia/utils/Utils.kt
+++ app/src/main/java/ru/netology/nmedia/utils/Utils.kt
@@ -10,6 +10,6 @@ fun formatCount(count: Int): String {
         in (10_000..999_999) -> "${floor(count / 1000.0).toInt()}K"
         in (1_000_000..1_099_999) -> "1M"
         in (1_100_000..Int.MAX_VALUE) -> "${(floor(count / 100_000.0) / 10.0)}M"
-        else -> throw NumberFormatException("Количество лайков не может быть отрицательным!")
+        else -> "0"
     }
 }
