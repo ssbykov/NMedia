799a055cfc8a6773093e2824b2dcb01abfe9b0ea
Sergei Bykov <ss.bykoff@gmail.com>
2024-01-27 19:00:00 +0300

    добавлена возможность добавления поста

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,12 +1,14 @@
 package ru.netology.nmedia
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.adapter.SetupClickListeners
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.utils.AndroidUtils
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,9 +31,23 @@ class MainActivity : AppCompatActivity() {
                 viewModel.removeById(post.id)
             }
         })
-        binding.root.adapter = adapter
+        binding.list.adapter = adapter
         viewModel.data.observe(this) { posts ->
-            adapter.submitList(posts)
+            val newPost = adapter.currentList.size < posts.size && adapter.currentList.size > 0
+            adapter.submitList(posts) {
+                if (newPost) binding.list.smoothScrollToPosition(0)
+            }
+        }
+        binding.save.setOnClickListener {
+            val text = binding.content.text.toString().trim()
+            if (text.isEmpty()) {
+                Toast.makeText(this, R.string.error_empty_content, Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+            viewModel.changeContactAndSave(text)
+            binding.content.setText("")
+            binding.content.clearFocus()
+            AndroidUtils.hideKeyboard(it)
         }
     }
 }
--- app/src/main/java/ru/netology/nmedia/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/PostViewModel.kt
@@ -1,13 +1,33 @@
 package ru.netology.nmedia
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.netology.nmedia.dto.Post
+
+
+val empty = Post(
+    id = 0,
+    author = "",
+    content = "",
+    published = "",
+    likedByMe = false
+)
 
 class PostViewModel : ViewModel() {
 
     private val repository = PostRepositoryInMemoryImpl()
     val data = repository.getAll()
+    private val edited = MutableLiveData(empty)
+
     fun likeById(id: Long) = repository.likeById(id)
     fun shareById(id: Long) = repository.shareById(id)
     fun removeById(id: Long) = repository.removeById(id)
-
+    fun changeContactAndSave(content: String) {
+        edited.value?.let {
+            if (it.content != content) {
+                repository.save(it.copy(content = content))
+                edited.value = empty
+            }
+        }
+    }
 }
