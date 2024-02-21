19f2e09dba0f35f99345960a82794097cbb7d8f0
Sergei Bykov <ss.bykoff@gmail.com>
2024-02-21 15:24:40 +0300

    реализован вариант SharedPreferences

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -1,9 +1,10 @@
 package ru.netology.nmedia.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import ru.netology.nmedia.dto.Post
-import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
+import ru.netology.nmedia.repository.PostRepositorySharedPrefsIn
 
 
 val empty = Post(
@@ -14,17 +15,22 @@ val empty = Post(
     likedByMe = false
 )
 
-class PostViewModel : ViewModel() {
+class PostViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val repository = PostRepositoryInMemoryImpl()
+    private val repository = PostRepositorySharedPrefsIn(application)
     val data = repository.getAll()
     val edited = MutableLiveData(empty)
 
     fun likeById(id: Long) = repository.likeById(id)
     fun shareById(id: Long) = repository.shareById(id)
     fun removeById(id: Long) = repository.removeById(id)
-    fun edit(post: Post) { edited.value = post }
-    fun clear() { edited.value = empty }
+    fun edit(post: Post) {
+        edited.value = post
+    }
+
+    fun clear() {
+        edited.value = empty
+    }
 
     fun changeContentAndSave(content: String) {
         edited.value?.let {
