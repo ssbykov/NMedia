6ead0049de1b86e2e7d4353bd82c647459121dc2
Sergei Bykov <ss.bykoff@gmail.com>
2024-02-25 23:49:50 +0300

    добавлен просмотр одного поста, устранена проблема запуска нескольких копий приложения через внешний интент

--- app/src/main/java/ru/netology/nmedia/activity/FeedFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/FeedFragment.kt
@@ -10,7 +10,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import ru.netology.nmedia.R
-import ru.netology.nmedia.activity.NewPostFragment.Companion.textPostID
 import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.adapter.PostsSetupClickListeners
 import ru.netology.nmedia.databinding.FragmentFeedBinding
