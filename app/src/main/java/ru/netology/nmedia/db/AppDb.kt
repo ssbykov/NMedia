40461f0c63b1da8e85f58526e760679db25e44b1
Sergei Bykov <ss.bykoff@gmail.com>
2024-05-22 11:12:37 +0300

    добавлен конвертер

--- app/src/main/java/ru/netology/nmedia/db/AppDb.kt
+++ app/src/main/java/ru/netology/nmedia/db/AppDb.kt
@@ -4,10 +4,22 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 import ru.netology.nmedia.dao.PostDao
+import ru.netology.nmedia.dto.AttachmentType
 import ru.netology.nmedia.entity.PostEntity
 
+class Converters {
+    @TypeConverter
+    fun toAttachmentType(value: String) = enumValueOf<AttachmentType>(value)
+
+    @TypeConverter
+    fun fromAttachmentType(value: AttachmentType) = value.name
+}
+
 @Database(entities = [PostEntity::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class AppDb : RoomDatabase() {
     abstract fun postDao(): PostDao
 
