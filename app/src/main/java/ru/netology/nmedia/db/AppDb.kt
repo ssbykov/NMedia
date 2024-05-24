47f00dc9c759a45775542b5c7ff69a05aba1a8e9
Sergei Bykov <ss.bykoff@gmail.com>
2024-05-24 17:23:01 +0300

    убрал конвертеры, работае без них

--- app/src/main/java/ru/netology/nmedia/db/AppDb.kt
+++ app/src/main/java/ru/netology/nmedia/db/AppDb.kt
@@ -10,16 +10,9 @@ import ru.netology.nmedia.dao.PostDao
 import ru.netology.nmedia.dto.AttachmentType
 import ru.netology.nmedia.entity.PostEntity
 
-class Converters {
-    @TypeConverter
-    fun toAttachmentType(value: String) = enumValueOf<AttachmentType>(value)
 
-    @TypeConverter
-    fun fromAttachmentType(value: AttachmentType) = value.name
-}
 
 @Database(entities = [PostEntity::class], version = 1)
-@TypeConverters(Converters::class)
 abstract class AppDb : RoomDatabase() {
     abstract fun postDao(): PostDao
 
